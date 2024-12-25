f917c0a71b31b143206cdff0e55b3a09eb00f165
LucasRgo <159014349+LucasRgo@users.noreply.github.com>
2024-12-25 13:18:26 +0000

    Wed, Dec 25, 2024, 10:18 AM -03:00

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,26 +12,45 @@ function Footer() {
                             &copy; 2024 Coded by <a href="https://lucasrgoveia.tech/" className="lucas">Lucas R Goveia</a>, Inc. All rights reserved.
                         </h5>
                     </div>
+
                     <div className="col-md-3" id="footer-social-media">
                         <h4 className="footer-title">Social media</h4>
                         <ul className="footer-list list-unstyled">
                             <li className="footer-list-item">
-                                <a href="https://www.linkedin.com/in/lucasrgoveiadev/" className="footer-link"><i className="bi bi-linkedin footer-icon"></i>LinkedIn</a>
+                                <a href="https://www.linkedin.com/in/lucasrgoveiadev/"
+                                className="footer-link"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                    <i className="bi bi-linkedin footer-icon"></i>LinkedIn
+                                </a>
                             </li>
                             <li className="footer-list-item">
-
-                                <a href="https://github.com/LucasRgo?tab=repositories" className="footer-link"><i className="bi bi-github footer-icon"></i>GitHub</a>
+                                <a href="https://github.com/LucasRgo?tab=repositories"
+                                className="footer-link"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                    <i className="bi bi-github footer-icon"></i>GitHub
+                                </a>
                             </li>
                             <li className="footer-list-item">
-
-                                <a href="mailto:lucas.lrg.0005@gmail.com" className="footer-link"><i className="bi bi-envelope footer-icon"></i>E-mail</a>
+                                <a href="mailto:lucas.lrg.0005@gmail.com"
+                                className="footer-link"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                    <i className="bi bi-envelope footer-icon"></i>E-mail
+                                </a>
                             </li>
                             <li className="footer-list-item">
-
-                                <a href="https://www.instagram.com/lucass_cj/" className="footer-link"><i className="bi bi-instagram footer-icon"></i>Instagram</a>
+                                <a href="https://www.instagram.com/lucass_cj/"
+                                className="footer-link"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                    <i className="bi bi-instagram footer-icon"></i>Instagram
+                                </a>
                             </li>
                         </ul>
                     </div>
+
                     <div className="col-md-3" id="footer-reference">
                         <h4 className="footer-title">Reference</h4>
                         <ul className="footer-list list-unstyled">
