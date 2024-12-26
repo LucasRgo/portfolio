39f57a0f0010358512690d34654119d4014d029b
LucasRgo <159014349+LucasRgo@users.noreply.github.com>
2024-12-26 12:53:24 +0000

    Thu, Dec 26, 2024, 9:53 AM -03:00

--- src/components/contactButton.jsx
+++ src/components/contactButton.jsx
@@ -1,10 +1,16 @@
 import './contactButton.css';
-
+import React, { useState } from 'react';
 
 const ContactButton = () => {
+    const [isCardOpen , setIsCardOpen]= useState(false);
+
+    const handleToggleCard = () => {
+        setIsCardOpen(!isCardOpen);
+      };
+
     return (
-        <button className="floating-contact-button">
-            Contact me here
+        <button className="floating-contact-button" onClick={handleToggleCard}>
+            {isCardOpen ? 'Close' : 'Contact me here'}
         </button>
     );
 };
